a909a6a44321b70c78df742141136ee34ba53e7d
Marco Montanari <sirmmo@gmail.com>
2018-07-07 11:33:46 +0200

    varaiable naming fixes

--- ingmmo/src/app/app.component.ts
+++ ingmmo/src/app/app.component.ts
@@ -8,26 +8,26 @@ import { IngmmoService } from './ingmmo.service';
 })
 export class AppComponent implements OnInit{
 
-  book_list;
-  conf_list;
-  game_list;
-  post_list;
-  less_list;
-  prj_list;
   sms_list;
+  post_list;
+  conf_list;
+  prj_list;
+  less_list;
+  book_list;
+  game_list;
 
   constructor(
     private ingmmo: IngmmoService
   ) { }
 
   ngOnInit() {
-    this.ingmmo.books.subscribe(data =>    {this.book_list = data; });
-    this.ingmmo.confs.subscribe(data =>    {this.conf_list = data; });
-    this.ingmmo.games.subscribe(data =>    {this.game_list = data; });
+    this.ingmmo.socials.subscribe(data =>  {this.sms_list  = data; });
     this.ingmmo.posts.subscribe(data =>    {this.post_list = data; });
+    this.ingmmo.confs.subscribe(data =>    {this.conf_list = data; });
     this.ingmmo.projects.subscribe(data => {this.prj_list  = data; });
     this.ingmmo.lessons.subscribe(data =>  {this.less_list = data; });
-    this.ingmmo.socials.subscribe(data =>  {this.sms_list  = data; });
+    this.ingmmo.books.subscribe(data =>    {this.book_list = data; });
+    this.ingmmo.games.subscribe(data =>    {this.game_list = data; });
   }
 
 }
