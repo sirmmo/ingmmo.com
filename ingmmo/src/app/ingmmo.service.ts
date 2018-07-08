afa56e3d60b231c00c8b0d51fc15630ace7cb472
Marco Montanari <sirmmo@gmail.com>
2018-07-08 10:14:07 +0200

    relativization

--- ingmmo/src/app/ingmmo.service.ts
+++ ingmmo/src/app/ingmmo.service.ts
@@ -12,34 +12,34 @@ export class IngmmoService {
   ) { }
 
   get books(): Observable<any> {
-    return this.http.get('/assets/books.json');
+    return this.http.get('assets/books.json');
   }
 
   get confs(): Observable<any> {
-    return this.http.get('/assets/conferences.json');
+    return this.http.get('assets/conferences.json');
   }
 
   get games(): Observable<any> {
-    return this.http.get('/assets/games.json');
+    return this.http.get('assets/games.json');
   }
 
   get posts(): Observable<any> {
-    return this.http.get('/assets/posts.json');
+    return this.http.get('assets/posts.json');
   }
 
   get lessons(): Observable<any> {
-    return this.http.get('/assets/lessons.json');
+    return this.http.get('assets/lessons.json');
   }
 
   get projects(): Observable<any> {
-    return this.http.get('/assets/projects.json');
+    return this.http.get('assets/projects.json');
   }
 
   get socials(): Observable<any> {
-    return this.http.get('/assets/socials.json');
+    return this.http.get('assets/socials.json');
   }
 
   get boardgames(): Observable<any> {
-    return this.http.get('/assets/boardgames.json');
+    return this.http.get('assets/boardgames.json');
   }
 }
